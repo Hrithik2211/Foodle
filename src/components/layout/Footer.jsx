c2e46d7dbe3bcd1b9e078b3ae2bf726079277cbf
Hrithik Yadav <hrithik22lko@gmail.com>
2024-10-28 20:08:34 +0530

    tried fixing footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,12 @@
-import {React} from 'react';
+import React from 'react';
 
 const Footer = () => {
 return (
-    <footer
-        className='bg-gradient-to-r from-pink-500 to-purple-700 text-white mt-auto w-full transition-opacity duration-500 hover:opacity-0'
-    >
-        <div className="container mx-auto px-4 py-6 text-center">
-            <p>&copy; 2024 Foodie App. All rights reserved.</p>
-        </div>
-    </footer>
+  <footer className="bg-gradient-to-r from-pink-500 to-purple-700 text-white  w-full mt-auto">
+    <div className="container mx-auto px-4 py-6 text-center">
+      <p>&copy; 2024 Foodie App. All rights reserved.</p>
+    </div>
+  </footer>
 );
 };
 export default Footer;
